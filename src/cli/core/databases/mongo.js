047368ca091fac74a16585fcb88b7b0a13bdd74d
Edouard <erucquoy@student.42.fr>
2018-10-12 20:17:49 +0200

    mongo database connection

--- src/cli/core/databases/mongo.js
+++ src/cli/core/databases/mongo.js
@@ -16,6 +16,9 @@ module.exports = {
   connectToServer: function( callback ) {
     if (!url) { return callback('Missing mongo URL')}
     MongoClient.connect( url, options, function( err, client ) {
+      if (!client || err) {
+        return callback(err);
+      }
       _db = client.db('abecms_dev');
       return callback( err );
     } );
