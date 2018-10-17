6284b3f209dc1942b27d79c9f551428b7fcd6212
Edouard <erucquoy@student.42.fr>
2018-10-17 14:08:20 +0200

    GET /abe/rest/templates

--- src/server/routes/rest/index.js
+++ src/server/routes/rest/index.js
@@ -2,5 +2,12 @@ import post from './post'
 import posts from './posts'
 import activityStream from './activity-stream'
 import authenticate from './authenticate'
+import getTemplates from './getTemplates'
 
-export {post, posts, activityStream, authenticate}
+export {
+    post,
+    posts,
+    activityStream,
+    authenticate,
+    getTemplates,
+}
