eb855eec3f334da9f1bde9cc9dece41556834ccc
Edouard <erucquoy@student.42.fr>
2018-11-19 14:24:31 +0100

    better REST workflows

--- src/server/rest/workflows/workflowController.js
+++ src/server/rest/workflows/workflowController.js
@@ -1,5 +1,6 @@
 import {
 	config,
+	User,
 } from '../../../cli'
 
 export const getWorkflows = (req, res) => {
@@ -21,6 +22,19 @@ export const getFullWorkflows = async (router, req, res, next) => {
 			regex: route.route.path.replace(/\*$/, '') + '.*'
 		})
 	})
+/*
+	if (
+        typeof routes[workflow] !== 'undefined' &&
+        routes[workflow] !== null
+      ) {
+        Array.prototype.forEach.call(routes[workflow], route => {
+          var reg = new RegExp(route)
+          if (reg.test(currentRoute)) {
+            isAllowed = true
+          }
+        })
+	  }
+	  */
 
 	var workflowUrl = {}
 	var previous = ''
@@ -28,62 +42,79 @@ export const getFullWorkflows = async (router, req, res, next) => {
 	Array.prototype.forEach.call(config.users.workflow, flow => {
 		var current = false
 		if (flow != 'publish') {
-		Array.prototype.forEach.call(config.users.workflow, flowCheck => {
-			if (current) {
-			nextWorkflow = flowCheck
-			current = false
-			}
-			if (flow === flowCheck) {
-			current = true
-			}
-		})
+			Array.prototype.forEach.call(config.users.workflow, flowCheck => {
+				if (current) {
+					nextWorkflow = flowCheck
+					current = false
+				}
+				if (flow === flowCheck) {
+					current = true
+				}
+			})
 		} else {
-		nextWorkflow = 'draft'
+			nextWorkflow = 'draft'
 		}
-		workflowUrl[flow] = [
-		{
-			url: `/abe/operations/edit/${flow}`,
-			action: 'edit',
-			workflow: flow,
-			previous: previous,
-			next: nextWorkflow
-		},
-		{
-			url: `/abe/operations/delete/${flow}`,
-			action: 'delete',
-			workflow: flow,
-			previous: previous,
-			next: nextWorkflow
-		},
-		{
-			url: `/abe/operations/submit/${flow}`,
-			action: 'submit',
-			workflow: flow,
-			previous: previous,
-			next: nextWorkflow
+		workflowUrl[flow] = []
+
+		if (User.utils.isUserAllowedOnRoute(res.user.role.workflow, `/abe/operations/edit/${flow}`)) {
+			workflowUrl[flow].push({
+				url: `/abe/operations/edit/${flow}`,
+				action: 'edit',
+				workflow: flow,
+				previous: previous,
+				next: nextWorkflow
+			})
 		}
-		]
+
+		if (User.utils.isUserAllowedOnRoute(res.user.role.workflow, `/abe/operations/delete/${flow}`)) {
+			workflowUrl[flow].push({
+				url: `/abe/operations/delete/${flow}`,
+				action: 'delete',
+				workflow: flow,
+				previous: previous,
+				next: nextWorkflow
+			})
+		}
+
+		if (User.utils.isUserAllowedOnRoute(res.user.role.workflow, `/abe/operations/submit/${flow}`)) {
+			workflowUrl[flow].push({
+				url: `/abe/operations/submit/${flow}`,
+				action: 'submit',
+				workflow: flow,
+				previous: previous,
+				next: nextWorkflow
+			})
+		}
+
 
 		if (flow !== 'draft') {
-		workflowUrl[flow].push({
-			url: `/abe/operations/reject/${flow}`,
-			action: 'reject',
-			workflow: flow,
-			previous: previous,
-			next: nextWorkflow
-		})
+			if (User.utils.isUserAllowedOnRoute(res.user.role.workflow, `/abe/operations/reject/${flow}`)) {
+				workflowUrl[flow].push({
+					url: `/abe/operations/reject/${flow}`,
+					action: 'reject',
+					workflow: flow,
+					previous: previous,
+					next: nextWorkflow
+				})
+			}
+
+			if (User.utils.isUserAllowedOnRoute(res.user.role.workflow, `/abe/api/pages/unpublish`)) {
+				workflowUrl[flow].push({
+					url: `/abe/api/pages/unpublish`,
+					action: 'unpublish',
+					workflow: flow,
+					previous: previous,
+					next: nextWorkflow
+				})
+			}
 		}
 		previous = flow
 	})
 
 	return res.json({
-		urls: urls,
-		// user: res.user,
-		config: config,
 		roles: config.users.roles,
-		workflow: config.users.workflow,
-		isWorkflow: 1,
-		workflowUrl: workflowUrl
+		workflows: config.users.workflow,
+		workflowsData: workflowUrl
 	})
 	
 }
