2b147f19913153407807bf83c92df2822d64f60d
Edouard <erucquoy@student.42.fr>
2018-11-26 14:04:05 +0100

    cleaning some dependance & scripts

--- src/server/public/abecms/scripts/v4/references.js
+++ src/server/public/abecms/scripts/v4/references.js
@@ -14,7 +14,6 @@ document.loadReference = loadReference;
 document.saveReference = saveReference;
 
 function createReferenceListItem (refer) {
-	console.log(refer)
 	var htmlStuff = `
 	<a href="#" class="list-group-item list-group-item-action" onclick="loadReference('${ refer }')">
 		${ refer }
@@ -40,7 +39,7 @@ function addReference() {
 
 function createReference (filename) {
 	var self = this
-	axios({
+	$.ajax({
 		url: '/abe/api/references',
 		method: 'POST',
 		data: {
@@ -48,10 +47,10 @@ function createReference (filename) {
 			json: JSON.stringify({})
 		}
 	})
-	.then(function(res) {
+	.done(function(res) {
 		self.list.push(filename)
 	})
-	.catch(function(err) {
+	.fail(function(err) {
 		console.log(err.data)
 	})
 }
@@ -62,7 +61,6 @@ function getReference(name) {
 		url: '/abe/api/references/' + name,
 	})
 	.done(function(res) {
-		console.log(res)
 		$('#jsonToShow').val(JSON.stringify(res, null, 2))
 	})
 	.fail(function(err) {
@@ -109,7 +107,6 @@ function initReferences () {
 	.done(function(res) {
 		references = res.references
 		generateList()
-		console.log(references)
 	})
 	.fail(function(err) {
 		console.log(err)
@@ -117,118 +114,3 @@ function initReferences () {
 }
 
 initReferences()
-
-
-/*
-var referencesApp = new Vue({
-	el: '#references',
-	delimiters: ['${', '}'],
-	data: {
-		list: [],
-		references: {},
-		jsonToShow: '',
-		jsonName: '',
-		showing: false,
-		newReference: '',
-		showSaveSuccess: false,
-	},
-	methods: {
-		getList () {
-			var self = this;
-			axios({
-				url: '/abe/api/references'
-			})
-			.then(function(res) {
-				self.list = res.data.references
-			})
-			.catch(function(err) {
-
-			})
-		},
-		getReference (name) {
-			var self = this;
-			axios({
-				url: '/abe/api/references/' + name,
-			})
-			.then(function(res) {
-				const references = self.references;
-				references[name] = res.data
-				self.references = references
-				self.jsonToShow = JSON.stringify(res.data, null, 2)
-				self.jsonName = name;
-				self.showing = true;
-			})
-			.catch(function(err) {
-				console.log(err)
-			})
-		},
-		saveReference () {
-			this.updateReference(this.jsonName, this.jsonToShow)
-		},
-		updateReference (filename, json) {
-			var self = this;
-			axios({
-				url: '/abe/api/references',
-				method: 'POST',
-				data: {
-					url: filename,
-					json: json
-				}
-			})
-			.then(function(res) {
-				self.showSaveSuccess = true
-				setTimeout(function() {
-					self.showSaveSuccess = false
-				}, 2000)
-			})
-			.catch(function(err) {
-				console.log(err)
-			})
-		},
-		createReference (filename) {
-			var self = this
-			axios({
-				url: '/abe/api/references',
-				method: 'POST',
-				data: {
-					url: filename,
-					json: JSON.stringify({})
-				}
-			})
-			.then(function(res) {
-				self.list.push(filename)
-			})
-			.catch(function(err) {
-				console.log(err.data)
-			})
-		},
-		addReference () {
-			let filename = this.newReference;
-			if (filename.indexOf('.json') === -1) {
-				filename = filename + '.json'
-			}
-			this.createReference(filename)
-		},
-		loadReference (name) {
-			this.getReference(name)
-		},
-		closeEditor () {
-			this.showing = false
-		}
-	},
-	created () {
-		
-		var self = this
-		axios({
-			url: '/abe/api/references'
-		})
-		.then(function(res) {
-			self.references = res.data.references
-			console.log(res.data)
-		})
-		.catch(function(err) {
-			console.log(err)
-		})
-	}
-})
-*/
