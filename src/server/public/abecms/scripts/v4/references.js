c814decdb85455c57310e31c4625fd07e6cfbd2e
Edouard <erucquoy@student.42.fr>
2018-11-26 13:59:03 +0100

    refactoring references vuejs to jquery

--- src/server/public/abecms/scripts/v4/references.js
+++ src/server/public/abecms/scripts/v4/references.js
@@ -1,3 +1,125 @@
+var refsList = $('#refs-list')
+var nameInput = $('#ref-filename')
+var editorcol = $('#editorcol')
+var filename = $('#filename')
+var filename2 = $('#filename2')
+var successAlert = $('#successalert')
+
+editorcol.hide()
+successAlert.hide()
+
+var references;
+
+document.loadReference = loadReference;
+document.saveReference = saveReference;
+
+function createReferenceListItem (refer) {
+	console.log(refer)
+	var htmlStuff = `
+	<a href="#" class="list-group-item list-group-item-action" onclick="loadReference('${ refer }')">
+		${ refer }
+	</a>`
+
+	return $(htmlStuff)
+}
+
+function generateList () {
+	for (var i in references) {
+		var ref = references[i]
+		refsList.append(createReferenceListItem(ref))
+	}
+}
+
+function addReference() {
+	let filename = nameInput.val()
+	if (filename.indexOf('.json') === -1) {
+		filename = filename + '.json'
+	}
+	createReference(filename)
+}
+
+function createReference (filename) {
+	var self = this
+	axios({
+		url: '/abe/api/references',
+		method: 'POST',
+		data: {
+			url: filename,
+			json: JSON.stringify({})
+		}
+	})
+	.then(function(res) {
+		self.list.push(filename)
+	})
+	.catch(function(err) {
+		console.log(err.data)
+	})
+}
+
+function getReference(name) {
+	var self = this;
+	$.ajax({
+		url: '/abe/api/references/' + name,
+	})
+	.done(function(res) {
+		console.log(res)
+		$('#jsonToShow').val(JSON.stringify(res, null, 2))
+	})
+	.fail(function(err) {
+		console.log(err)
+	})
+}
+
+function loadReference (name) {
+	editorcol.show()
+	filename.html(name)
+	filename2.html(name)
+	getReference(name)
+}
+
+function saveReference () {
+	updateReference(filename.html(), $('#jsonToShow').val())
+}
+
+function updateReference (filename, json) {
+	$.ajax({
+		url: '/abe/api/references',
+		method: 'POST',
+		data: {
+			url: filename,
+			json: json
+		}
+	})
+	.done(function(res) {
+		successAlert.show()
+		setTimeout(function() {
+			successAlert.hide()
+		}, 2000)
+		
+	})
+	.fail(function(err) {
+		console.log(err)
+	})
+}
+
+function initReferences () {
+	$.ajax({
+		url: '/abe/api/references'
+	})
+	.done(function(res) {
+		references = res.references
+		generateList()
+		console.log(references)
+	})
+	.fail(function(err) {
+		console.log(err)
+	})
+}
+
+initReferences()
+
+
+/*
 var referencesApp = new Vue({
 	el: '#references',
 	delimiters: ['${', '}'],
@@ -109,3 +231,4 @@ var referencesApp = new Vue({
 		})
 	}
 })
+*/
