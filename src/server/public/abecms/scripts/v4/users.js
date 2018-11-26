3bdd9d7d5e6ebd3f8fb334713252be9f3249589f
Edouard <erucquoy@student.42.fr>
2018-11-26 11:44:12 +0100

    refactoring vuejs to jquery

--- src/server/public/abecms/scripts/v4/users.js
+++ src/server/public/abecms/scripts/v4/users.js
@@ -1,238 +1,324 @@
-var usersList = new Vue({
-	el: '#users-list',
-	delimiters: ['${', '}'],
-	data: { 
-		users: [],
-		currentUser: {},
-		currentIndex: -1,
-		currentRole: 'No',
-		message: 'Coucou',
-		roles: [
-			{ name: 'Admin', workflow: 'admin'},
-			{ name: 'Writer', workflow: 'writer'}
-		],
-		userForm: {
-			role: '',
-		},
-		showSuccessUpdate: false,
-		removeIndex: -1,
-		removeUserData: {},
-		newUser: {
-			name: '',
-			username: '',
-			role: null,
-			roleString: '',
-			email: '',
-			password: '',
-		},
-		showSuccessAdd: false,
-		showErrorAdd: false,
-		textErrorAdd: '',
-	},
-	computed: {
-		usersList() {
-			return this.users
-		}
-	},
-	methods: {
-		openModifyModal (id) {
-			for (var i in this.users) {
-				if (this.users[i].id === id) {
-					this.currentIndex = i;
-					this.currentUser = this.users[i]
-					this.currentRole = (this.currentUser.role.name)
-					this.userForm = JSON.parse(JSON.stringify(this.currentUser))
-					this.userForm.password = null
-					this.userForm.role = this.currentUser.role.name;
-					break ;
-				}
-			}
-			$('#modifyModal').modal('show')
-		},
-		onAddUserSubmit (evt) {
-			evt.preventDefault();
-			var self = this
-			var { password, username, name, email, role, roleString } = this.newUser
-			if (typeof roleString === 'string') {
-				for (var i in this.roles) {
-					if (this.roles[i].name == roleString) {
-						role = this.roles[i]
-						this.newUser.role = role;
-					}
-				}
-			}
-			axios({
-				url: '/abe/api/users',
-				method: 'POST',
-				data: {
-					username: username,
-					name: name,
-					email: email,
-					role: role,
-					password: password,
-				}
-			})
-			.then(function (res) {
-				self.users.push(res.data.user)
-			})
-			.catch (function (err) {
-				self.handleErrorAdd(err)
-			})
-		},
-		onUpdateUserSubmit (evt) {
-			evt.preventDefault();
-			var self = this
-			var { username, name, email, role, id } = this.userForm
-			if (typeof role === 'string') {
-				for (var i in this.roles) {
-					if (this.roles[i].name == role) {
-						role = this.roles[i]
-						this.userForm.role = role;
-					}
-				}
-			}
-			axios({
-				url: '/abe/api/users',
-				method: 'PUT',
-				data: {
-					id: id,
-					username: username,
-					name: name,
-					email: email,
-					role: role,
-				}
-			})
-			.then(function(res) {
-				self.updateUsersMore(id, self.userForm)
-				self.showSuccessUpdate = true;
-				setTimeout(function () {
-					self.showSuccessUpdate = false
-				}, 3000)
-			}).catch(function(err) {
-				console.log(err)
-			})
-		},
-		deactivateUser (id) {
-			var self = this
-			axios({
-				url: '/abe/api/users/deactivate',
-				method: 'PUT',
-				data: {
-					id: id,
-				}
-			})
-			.then(function(res) {
-				self.updateUsers(id, 'actif', 0)
-			})
-			.catch(function(err) {
-				console.log(err)
-			})
-		},
-		activateUser (id) {
-			var self = this
-			axios({
-				url: '/abe/api/users/activate',
-				method: 'PUT',
-				data: {
-					id: id,
-				}
-			})
-			.then(function(res) {
-				self.updateUsers(id, 'actif', 1)
-			})
-			.catch(function(err) {
-				console.log(err)
-			})
-		},
-		updateUsers (id, prop, val) {
-			const users = this.users;
-			for (var i in users) {
-				if (users[i].id === id) {
-					users[i][prop] = val;
-				}
-			}
-			this.users = users;
-		},
-		updateUsersMore (id, props) {
-			const users = this.users;
-			for (var i in users) {
-				if (users[i].id === id) {
-					for (var prop in props) {
-						users[i][prop] = props[prop];
-					}
-				}
-			}
-			this.users = users;
-		},
-		askRemoveUser (id) {
-			for (var i in this.users) {
-				if (this.users[i].id === id) {
-					this.removeIndex = i;
-					this.removeUserData = this.users[i]
-				}
-			}
-			$('#removeModal').modal('show')
-		},
-		handleErrorAdd (err) {
-			this.showErrorAdd = true
-			if (err.response) {
-				this.textErrorAdd = err.response.data.message
-			} else {
-				this.textErrorAdd = 'No connection ?'
-			}
-		},
-		tryRemoveUser (id) {
-			var self = this
-			axios({
-				url: '/abe/api/users/' + id,
-				method: 'DELETE'
-			})
-			.then(function(res) {
-				self.removeUser(id)
-			})
-			.catch(function(err) {
-				console.log(err)
-			})
-		},
-		removeUser (id) {
-			var users = this.users;
-			for (var i in users) {
-				if (users[i].id == id) {
-					delete users[i]
-				}
-			}
-			this.users = users;
-			this.users.push()
-			this.users.pop()
-		},
-		openAddUser () {
-			$('#addModal').modal('show')
-		}
-	},
-	created () {
-		var self = this;
-		axios({
-			url: '/abe/api/users',
-		}).then(function(res) {
-			for (var i in res.data.users) {
-				self.users.push(res.data.users[i])
-			}
-		}).catch(function(err) {
-			console.log(err)
-		})
-		axios({
-			url: '/abe/api/roles',
-		}).then(function(res) {
-			self.roles = res.data.roles
-		}).catch(function(err) {
-			console.log(err)
-		})
+var usersList = $('#users-list')
+var listBody = $('#list-body')
+
+var usersElements = []
+var roles = []
+var users = {}
+
+var removeUserIndex = -1;
+var removeUserId = -1;
+
+var updateIndex = -1;
+var updateId = -1;
+function getUpdateId () {
+	return updateId
+}
+function setUpdateId (id) {
+	updateId = id
+}
+
+$('#alertAddError').hide()
+$('#alertAddSuccess').hide()
+$( "#alertModifyError" ).hide();
+
+
+initUsersList()
+
+function createUserItem (user) {
+
+	var htmlStuff = `<tr in usersList" id="${user.id}">
+	<td><span><img style="height:42px;" src="${user.avatar}" alt="Profile Picture" /></span></td>
+	<td class="cell-user">${ user.username }</td>
+	<td class="cell-user">${ user.name }</td>
+	<td class="cell-user">${ user.email }</td>
+	<td class="cell-user">${ user.role.name }</td>
+	<td class="cell-user">
+		${ (user.actif) ? `<span class="fas fa-eye-slash icon-deactivate text-danger" onclick="deactivateUser(${user.id})" data-toggle="tooltip" data-placement="top" title="Deactivate the user"></span>` : `<span class="fas fa-eye icon-activate text-info" onclick="activateUser(${user.id})" data-toggle="tooltip" data-placement="top" title="Activate the user"></span>` }
+	</td>
+	<td class="cell-user">
+		<span class="fas fa-pencil-alt text-primary icon-action"  onclick="openModifyModal(${user.id})"></span>&nbsp;&nbsp;
+		<span class="fas fa-trash text-danger icon-action" onclick="askRemoveUser(${user.id})"></span>
+	</td>
+</tr>`
+
+	return $(htmlStuff)
+}
+
+function generateList () {
+	listBody.empty()
+	for (var i in users) {
+		listBody.append(createUserItem(users[i]))
 	}
+}
+
+function deactivateUser (id) {
+	$.ajax({
+		url: '/abe/api/users/deactivate',
+		method: 'PUT',
+		data: {
+			id: id,
+		}
+	})
+	.done(function(res) {
+		users[findIndexWithId(id)].actif = 0;
+		generateList()
+
+	})
+	.fail(function(err) {
+		console.log(err)
+	})
+}
+
+function findIndexWithId (id) {
+	for (var i in users) {
+		if (users[i].id === id) {
+			return i
+		}
+	}
+}
+
+function onAddUserSubmit () {
+	var role = {}
+	var password = $('#inputAddPassword').val()
+	var username = $('#inputAddUsername').val()
+	var name = $('#inputAddName').val()
+	var email = $('#inputAddEmail').val()
+	var roleString = $('#inputAddRole').val()
+
+	if (typeof roleString === 'string') {
+		for (var i in roles) {
+			if (roles[i].name == roleString) {
+				role = roles[i]
+			}
+		}
+	}
+
+	$.ajax({
+		url: '/abe/api/users',
+		method: 'POST',
+		data: {
+			username: username,
+			name: name,
+			email: email,
+			role: role,
+			password: password,
+		}
+	})
+	.done(function (res) {
+		users.push(res.user)
+		generateList()
+		
+		closeAddUser()
+	})
+	.fail (function (err) {
+		console.log(err)
+		handleErrorAdd(err)
+	})
+}
+
+function onUpdateUserSubmit () {
+	
+	var role = {}
+	var id = getUpdateId()
+	var username = $('#inputModifyUsername').val()
+	var name = $('#inputModifyName').val()
+	var email = $('#inputModifyEmail').val()
+	var roleString = $('#inputModifyRole').val()
+
+	if (typeof roleString === 'string') {
+		for (var i in roles) {
+			if (roles[i].name == roleString) {
+				role = roles[i]
+			}
+		}
+	}
+
+	console.log(role, email, id)
+
+	$.ajax({
+		url: '/abe/api/users',
+		method: 'PUT',
+		data: {
+			id: id,
+			username: username,
+			name: name,
+			email: email,
+			role: role,
+		}
+	})
+	.done(function(res) {
+		updateUsersMore(id, { username, name, email, role })
+		setTimeout(function () {
+			// self.showSuccessUpdate = false
+		}, 3000)
+		$('#modifyModal').modal('hide')
+	}).fail(function(err) {
+		console.log(err)
+	})
+}
+
+function activateUser (id) {
+	$.ajax({
+		url: '/abe/api/users/activate',
+		method: 'PUT',
+		data: {
+			id: id,
+		}
+	})
+	.done(function(res) {
+		users[findIndexWithId(id)].actif = 1;
+		generateList()
+	})
+	.fail(function(err) {
+		console.log(err)
+	})
+}
+
+function updateUsers (id, prop, val) {
+	const users = this.users;
+	for (var i in users) {
+		if (users[i].id === id) {
+			users[i][prop] = val;
+		}
+	}
+	this.users = users;
+}
+
+function initUsersList() {
+	$.ajax({
+		url: '/abe/api/users'
+	}).done(function (res) {
+		console.log(res)
+		users = res.users
+		generateList()
+	}).fail(function (err) {
+		console.log(err);
+	});
+
+	$.ajax({
+		url: '/abe/api/roles'
+	}).done(function (res) {
+		roles = res.roles;
+		generateRolesList()
+	}).fail(function (err) {
+		console.log(err);
+	});
+}
+
+function askRemoveUser (id) {
+	for (var i in users) {
+		if (users[i].id === id) {
+			removeUserIndex = i;
+			removeUserId = users[i].id
+			generateDeleteModal (i, users[i])
+			break ;
+		}
+	}
+	$('#removeModal').modal('show')
+}
+
+function tryRemoveUser (id) {
+	$.ajax({
+		url: '/abe/api/users/' + id,
+		method: 'DELETE'
+	})
+	.done(function(res) {
+		delete users[removeUserIndex]
+		generateList()
+		closeDeleteUser()
+	})
+	.fail(function(err) {
+		console.log(err)
+	})
+}
+
+function generateDeleteModal (index, data) {
+	$('#mr-username').html(data.username)
+	$('#mr-username2').html(data.username)
+	$('#mr-name').html(data.name)
+	$('#mr-email').html(data.email)
+}
+
+function openAddUser () {
+	$('#addModal').modal('show')
+}
+
+function closeAddUser () {
+	$('#addModal').modal('hide')
+}
+
+function closeDeleteUser () {
+	$('#removeModal').modal('hide')
+}
+
+function generateRolesList () {
+	for (var i in roles) {
+		$('#inputAddRole').append($(`<option value="${roles[i].name}">${ roles[i].name }</option>`))
+		$('#inputModifyRole').append(`<option value="${roles[i].name}">${roles[i].name}</option>`)
+	}
+}
+
+$('#addUserForm').on('submit', function (evt) {
+	evt.preventDefault();
+	onAddUserSubmit()
 })
 
-Vue.component('profile-img', {
-	props: {
-		avatar: String,
-	},
-  	template: '<span><img style="height:42px;" :src="avatar" alt="Profile Picture" /></span>'
+$('#removeUserConfirmButton').on('click', function(evt) {
+	tryRemoveUser(removeUserId)
 })
+
+function openModifyModal (id) {
+	for (var i in users) {
+		if (parseInt(users[i].id) === parseInt(id)) {
+			var data = users[i]
+			$('#inputModifyEmail').val(data.email)
+			$('#inputModifyUsername').val(data.username)
+			$('#inputModifyName').val(data.name)
+			$('#inputStringRole').html(data.role.name)
+			$('#inputStringUsername').html(data.username)
+			updateIndex = i;
+			updateId = id;
+			setUpdateId(id)
+			break ;
+		}
+	}
+	$('#modifyModal').modal('show')
+}
+
+$('#formUserUpdate').on('submit', function(evt) {
+	evt.preventDefault();
+	onUpdateUserSubmit()
+})
+
+function updateUsersMore (id, props) {	
+	for (var i in users) {
+		if (parseInt(users[i].id) === parseInt(id)) {
+			for (var prop in props) {
+				users[i][prop] = props[prop];
+			}
+		}
+	}
+	generateList()
+}
+
+function handleErrorAdd (err) {
+	console.log(err)
+	$( "#textErrorAdd" ).html()
+	$( "#alertAddError" ).show();
+	if (err.responseJSON) {
+		$('#textErrorAdd').html(err.responseJSON.message)
+	} else {
+		$('#textErrorAdd').html('No connection ?')
+	}
+}
+
+function handleErrorModify (err) {
+	console.log(err)
+	$( "#textErrorModify" ).html()
+	$( "#alertModifyError" ).show();
+	if (err.responseJSON) {
+		$('#textErrorModify').html(err.responseJSON.message)
+	} else {
+		$('#textErrorModify').html('No connection ?')
+	}
+}
